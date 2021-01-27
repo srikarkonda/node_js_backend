276b2694ca1223a77a8aac1f031988eab0646b0a
Niharika Goli <niharika.goli@visiram.com>
2021-01-27 16:26:57 +0530

    node and mango setup changes

---
+++ app.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const express=require('express');
+const bodyParser = require('body-parser');
+const Demo=require('./models/demo')
+const app=express();
+app.use(bodyParser.json());
+app.use("/",express.static(path.join(__dirname,"angular")))
+
+
+//const postsRoutes = require("./routes/posts");
+
+const mongoose  = require('mongoose');
+
+
+mongoose.connect("mongodb+srv://niharika:niharika@cluster0.kgqdx.mongodb.net/node-angular?retryWrites=true&w=majority",
+{ useUnifiedTopology: true , useNewUrlParser: true, useCreateIndex: true  })
+.then(() => {
+  console.log("connected to database");
+})
+.catch(() => {
+  console.log("connection failed");
+});
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, content-type, Authorization, text/html, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.get('/api/getPosts',(req,res,next) =>{
+  // const data=new Demo({
+  //   title:"test1",
+  //   content:"srikar"
+  // });
+Demo.find().then((data)=>{
+  console.log(data);
+  res.status(200).json({
+    message: "Posts send succesfullyy",
+    mangoData:data
+  });
+})
+  
+});
+
+// app.use((req,res,next) => {
+//   res.sendFile(path.join(__dirname,"angular","index.html"));
+// });
+module.exports = app;
