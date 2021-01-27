71cb1457f8c495b703afadda4c21a36003ecaa55
srikarkonda <srikarkonda98@gmail.com>
2021-01-27 16:36:27 +0530

    git config changes

--- models/demo.js
+++ models/demo.js
@@ -1,8 +1,8 @@
 const mongoose= require('mongoose');
 
 const demoSchema = mongoose.Schema({
-//   title: { type: 'String', required: true},
-//   content: { type: 'String', required: true},
+  title: { type: 'String', required: true},
+  content: { type: 'String', required: true},
 });
 
 module.exports = mongoose.model('Demo',demoSchema);
